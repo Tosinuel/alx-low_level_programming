bdccdd6b88b27928357455074a3dec72f04169a7
Tosinuel <expressionandpuresound.com>
2023-02-24 02:33:08 -0800

    main.h

---
+++ 0x03-debugging/README.md
@@ -0,0 +1,2 @@
+This is a readme note.
+Thank you.
--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,15 +3,16 @@
 /**
  * print_numbers - prints the numbers, from 0 to 9,
  * followed by a new line
- */
-void print_numbers(void)
+ */void print_numbers(void)
 {
-	int i;
+    char n = 0;
 
-	for (i = 0; i < 10; i++)
-	{
-		_putchar(i + '0');
-	}
+    while (n <= 9) 
+    {
+     _putchar('0' + n);
+     n++;
+    }
 
-	_putchar('\n');
+    _putchar('\n');
 }
+
