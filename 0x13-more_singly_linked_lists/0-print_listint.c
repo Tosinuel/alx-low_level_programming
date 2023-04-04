36f63416b6699fa8e95f0e682adfa6c7e78b1386
Tosinuel <expressionandpuresound.com>
2023-04-04 08:32:16 -0700

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,22 +1,18 @@
 #include "lists.h"
-#include <stdio.h>
-
 /**
- * print_listint - prints all the elements of a linked list
- * @h: head of the list
- *
- * Return: the number of nodes
- */
+* print_listint - prints all the elements of a linked list
+* @h: linked list of type listint_t to print
+**
+Return: number of nodes
+*/
 size_t print_listint(const listint_t *h)
 {
-const listint_t *cursor = h;
-size_t count = 0;
-
-while (cursor != NULL)
+size_t num = 0;
+while (h)
 {
-printf("%d\n", cursor->n);
-count += 1;
-cursor = cursor->next;
+printf("%d\n", h->n);
+num++;
+h = h->next;
 }
-return (count);
+return (num);
 }
--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,34 +1,34 @@
-#include "lists.h"
 #include <stdlib.h>
-
+#include "lists.h"
 /**
- * free_listint_safe - frees a list
- * @h: the pointer to the list to free
- * Return: the size of free'd list
- */
+* free_listint_safe - frees a linked list
+* @h: pointer to the first node in the linked list
+**
+Return: number of elements in the freed list
+*/
 size_t free_listint_safe(listint_t **h)
 {
-	size_t nodes = 0;
-	listint_t *temp = NULL;
-
-	if (!(h && *h))
-		return (nodes);
-
-	while (*h)
-	{
-		nodes++;
-		if (*h > (*h)->next)
-		{
-			temp = *h;
-			*h = (*h)->next;
-			free(temp);
-		}
-		else
-		{
-			free(*h);
-			*h = NULL;
-		}
-	}
-	*h = NULL;
-	return (nodes);
+size_t len = 0;
+int diff;
+listint_t *temp;
+if (!h || !*h)
+return (0);
+while (*h)
+{
+diff = *h - (*h)->next;
+if (diff > 0)
+{ temp =
+(*h)->next;
+free(*h);
+*h = temp;
+len++;
+}
+else
+{ free(*h);
+*h = NULL;
+len++;
+break;
+}}
+*h = NULL;
+return (len);
 }
